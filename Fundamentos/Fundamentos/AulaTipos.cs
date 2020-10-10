ac4047fd38eb06dc84d8285adc7c76619e9adc2c
Jhow Jhow <jhonatacandido@hotmail.com>
2020-10-10 01:16:54 -0300

    Update one

---
+++ Fundamentos/Fundamentos/AulaTipos.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Fundamentos
+{
+    class AulaTipos
+    {
+
+        /* Variável do tipo var possui seu tipo definido a partir do primeiro recebimento e depois 
+            * permanece com esse tipo até o encerramento do sistema */
+
+            #region Tipo Objetive
+
+            object obj = 100;
+            object Obj1 = "dsad";
+            #endregion
+
+
+            #region tipo Const
+            /*Utilizado para definir uma variável constante ou seja a partir que foi declarado o primeiro valor não poderá mais ser alterada*/
+
+            const string nome = "Jhonata";
+        // nome = "Diego"; // O sistema não permite alterar
+        #endregion
+
+        #region Tpo Enum
+
+
+        #endregion
+    }
+}
---
+++ Fundamentos/Fundamentos/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace Fundamentos
+{
+    class Program
+    {
+        enum Notas
+        {
+            Minimo,
+            Media,
+            Maxima 
+
+        }
+        static void Main(string[] args)
+        {
+
+            /* Variável do tipo var possui seu tipo definido a partir do primeiro recebimento e depois 
+                * permanece com esse tipo até o encerramento do sistema */
+
+            #region Tipo Objetive
+
+            object obj = 100;
+            object Obj1 = "dsad";
+            #endregion
+
+            Notas notasAlunos = Notas.Minimo;
+
+            #region tipo Const
+            /*Utilizado para definir uma variável constante ou seja a partir que foi declarado o primeiro valor não poderá mais ser alterada*/
+
+            const string nome = "Jhonata";
+            // nome = "Diego"; // O sistema não permite alterar
+            #endregion
+
+            #region Tpo Enum
+
+
+            #endregion
+
+
+        }
+    }
+}
