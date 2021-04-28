03f76f7d6bf5192bf295643fa21344b264547489
Jhow Jhow <jhonatacandido@hotmail.com>
2021-04-27 21:43:39 -0300

    Aula de modificadores.

---
+++ Fundamentos/Modificadores/Classes.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Modificadores
+{
+    class Teste
+    {
+        public string Nome;
+        public string Sobrenome;
+
+        public void Metodo1() 
+        { 
+        
+        }
+
+        public void Metodo2()
+        { 
+        
+        }
+    }
+
+    class Humano
+    {
+        // Protected semelhante ao private mas define que é particular da classe e de quem HERDAR
+        protected string nome;
+        private string sobrenome;
+        // INTERNAL DEFINE QUE A PROPRIEDADE É VALIDO SOMENTE PARA ESSA APLICAÇÃO, CASO SEU SISTEMA ESTEJA INTEGRADO COM OUTRAS COMPILAÇÕES
+        internal int idade;
+    }
+
+    class Homem : Humano
+    {
+        public void Metodo1()
+        {
+            nome = "";
+            idade = 1;
+        }
+    }
+
+
+}
---
+++ Fundamentos/heranca/Classes.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace heranca
+{
+    public class Humano
+    {
+
+        // Método virtual é semelhante ao abstrato porém já possui uma implementação base
+        // Método sealed é lacra/tranca o método para não ser mais modificado ou seja não será mais HERDADO
+        public virtual void Olhos()
+        {
+            Console.WriteLine("Humano.Olhos");
+        }
+
+        public virtual void Cabelos()
+        {
+            Console.WriteLine("Humano.Cabelos");
+        }
+
+    }
+
+    public class Pessoa : Humano 
+    {
+        //a assinatura Override define que é uma método que está sendo sobescrevido
+        public sealed override void Olhos()
+        {
+            Console.WriteLine("Pessoa.Olhos");
+        }
+
+        public override void Cabelos()
+        {
+            Console.WriteLine("Pessoa.Cabelos");
+        }
+
+    }
+
+    public class Homem : Pessoa
+    {
+        //public override void Olhos()
+        //{
+        //    Console.WriteLine("Homem.Olhos");
+        //}
+
+        public override void Cabelos()
+        {
+            Console.WriteLine("Homem.Cabelos");
+        }
+
+    }
+}
